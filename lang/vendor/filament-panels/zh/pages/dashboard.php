3cc5257b3335b6c15cbb6a17713b9252a31d8394
PuneetGOTO <an920513@gmail.com>
2026-07-08 11:46:26 +0800

    Translate Filament dashboard messages

---
+++ lang/vendor/filament-panels/zh/pages/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'title' => '儀表板',
+
+    'actions' => [
+
+        'filter' => [
+
+            'label' => '篩選',
+
+            'modal' => [
+
+                'heading' => '篩選',
+
+                'actions' => [
+
+                    'apply' => [
+
+                        'label' => '套用',
+
+                    ],
+
+                ],
+
+            ],
+
+        ],
+
+    ],
+
+];
